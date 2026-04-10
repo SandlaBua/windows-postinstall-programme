69c353d9d70c06b35027522a29583882729812e0
Benedikt Sandler <109026658+SandlaBua@users.noreply.github.com>
2026-04-10 23:22:27 +0200

    Update remote-network.ps1

--- profiles/remote-network.ps1
+++ profiles/remote-network.ps1
@@ -1,10 +1,7 @@
 . ([scriptblock]::Create((Invoke-RestMethod 'https://raw.githubusercontent.com/SandlaBua/windows-postinstall-programme/main/lib/common.ps1')))
 
 $packages = @(
-    @{ Id = 'MoonlightGameStreamingProject.Moonlight'; Scope = 'user';    PreKill = @('moonlight') }
-    @{ Id = 'LizardByte.Sunshine';                     Scope = 'machine'; PreKill = @('sunshine') }
     @{ Id = 'Parsec.Parsec';                          Scope = 'user';    PreKill = @('parsec', 'parsecd') }
-    @{ Id = 'Parsec.ParsecVDD';                       Scope = 'machine'; PreKill = @() }
     @{ Id = 'Tailscale.Tailscale';                    Scope = 'machine'; PreKill = @('tailscale', 'tailscale-ipn') }
 )
 
