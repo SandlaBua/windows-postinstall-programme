806771dd979cb698aa7d32173a6c6f01bc0c6afc
Benedikt Sandler <109026658+SandlaBua@users.noreply.github.com>
2026-04-10 20:56:52 +0200

    Update media-utility.ps1

--- profiles/media-utility.ps1
+++ profiles/media-utility.ps1
@@ -1,1 +1,17 @@
+. "$PSScriptRoot\..\lib\common.ps1"
 
+$packages = @(
+    @{ Id = '7zip.7zip'; Scope = 'machine'; PreKill = @('7zfm', '7zg') }
+    @{ Id = 'winaero.tweaker'; Scope = 'machine'; PreKill = @('winaerotweaker') }
+    @{ Id = 'GuinpinSoft.MakeMKV'; Scope = 'machine'; PreKill = @('makemkv') }
+    @{ Id = 'HandBrake.HandBrake'; Scope = 'machine'; PreKill = @('handbrake') }
+    @{ Id = 'Balena.Etcher'; Scope = 'machine'; PreKill = @('balenaetcher') }
+    @{ Id = 'WinSCP.WinSCP'; Scope = 'user'; PreKill = @('winscp') }
+    @{ Id = 'PowerSoftware.AnyBurn'; Scope = 'machine'; PreKill = @('anyburn') }
+    @{ Id = 'RaspberryPiFoundation.RaspberryPiImager'; Scope = 'machine'; PreKill = @('rpi-imager') }
+    @{ Id = 'Rufus.Rufus'; Scope = 'user'; PreKill = @('rufus') }
+)
+
+foreach ($pkg in $packages) {
+    Install-WingetPackage -Id $pkg.Id -Scope $pkg.Scope -PreKill $pkg.PreKill -SkipIfInstalled
+}
